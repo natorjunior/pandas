e68121263f6d94e4e15c934ec6979bc730811217
natorjunior <natorjuniorcc@gmail.com>
2022-10-22 15:26:15 -0300

    :recycle: Exemplo 02, respondendo outras mensagens

---
+++ Aula-05/bot_ex2.py
@@ -0,0 +1,26 @@
+import logging
+from telegram import Update
+from telegram.ext import *
+import random
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+msgs = ['Olá','Como vai?','Olá','de boa ','hi']
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Olá, eu sou o BOT, como vai?")
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    #Exemplo para enviar uma mensagem
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=msgs[random.randint(0,len(msgs)-1)]
+    )
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('TOKEN').build()
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.run_polling()
